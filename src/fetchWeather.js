a49623d714ede84a2d59c10d6bcba5b9da5c98a9
kenta asakura <asakura.kenta963420@gmail.com>
2024-11-02 22:53:12 +0900

    JS and HTML refactored. New JS file and importing refactored. New scss  created

---
+++ src/fetchWeather.js
@@ -0,0 +1,66 @@
+const apiKey = process.env.ACCUWEATHER_API_KEY;
+const selectedLocation = document.querySelector('.main-nav__location-btn');
+const currentWeather = document.querySelector('.current-weather');
+const currentWeatherCity = currentWeather.querySelector('.current-weather__city');
+const currentWeatherTemp = currentWeather.querySelector('.current-weather__temperature');
+const currentWeatherCondition = currentWeather.querySelector('.current-weather__condition');
+const currentWeatherTempHi = currentWeather.querySelector('.current-weather__temperature-range__high');
+const currentWeatherTempLo = currentWeather.querySelector('.current-weather__temperature-range__low');
+
+function fetchAndUpdateWeatherData(lat, lon) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // console.log(data.name);
+
+      // Extract data to HTML mark up
+      selectedLocation.textContent = data.name;
+      // currentWeatherCity.textContent = data.name;
+      currentWeatherTemp.textContent = `${Math.round(data.main.temp)}°`;
+      currentWeatherCondition.textContent = data.weather[0].main;
+      currentWeatherTempHi.textContent = `H:${Math.round(data.main.temp_max)}°`;
+      currentWeatherTempLo.textContent = `L:${Math.round(data.main.temp_min)}°`;
+    })
+    .catch(error => console.error('Error fetching the data:', error));
+};
+
+function searchCityCoords(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // console.log(data);
+
+      const coords = {
+        lat: data.coord.lat,
+        lon: data.coord.lon
+      };
+      // console.log(coords);
+
+      return coords;
+    })
+    .catch(error => console.error('Error fetching the data:', error));
+};
+searchCityCoords("Manila");
+
+function getCurrentLocationCoords() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      // console.log(fetchAndUpdateWeatherData(latitude, longitude));
+
+      fetchAndUpdateWeatherData(latitude, longitude)
+      // return { latitude, longitude };
+    }, error => {
+      console.error('Error getting geolocation:', error);
+    });
+  } else {
+    console.error('Geolocation is not supported by this browser.');
+  }
+};
+getCurrentLocationCoords();
---
+++ src/searchWeather.js
@@ -0,0 +1,7 @@
+const headerLocationBtn = document.querySelector('.main-nav__location-btn');
+
+headerLocationBtn.addEventListener('click', () => {
+  // console.log('hello from header');
+
+  
+});
