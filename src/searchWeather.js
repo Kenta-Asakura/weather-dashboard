ba97c1f39b6f16bb78d1069740cf869f3fca82a8
kenta asakura <asakura.kenta963420@gmail.com>
2024-11-05 23:59:33 +0900

    search panel in progress

--- src/searchWeather.js
+++ src/searchWeather.js
@@ -3,7 +3,7 @@ import { hideElement } from "./utils.js";
 
 const locationSearchPanel = document.querySelector(".location-search");
 const showLocationSearchBtn = document.querySelector(".main-nav__location-btn");
-const hideLocationSearchBtn = document.querySelector(".location-search__close-btn");
+const hideLocationSearchBtn = document.querySelector(".location-search__top-close-btn");
 
 showElement(showLocationSearchBtn, locationSearchPanel);
 hideElement(hideLocationSearchBtn, locationSearchPanel);
