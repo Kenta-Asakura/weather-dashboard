94db2417531450b2e63b344da15a01d5975637cd
Kenta Asakura <asakura.kenta963420@gmail.com>
2026-03-12 23:36:40 +0900

    feat(services): implement loadCities foundation
    - Add async method (loadCities) to fetch cities data with error handling and duplicate prevention

--- src/services/CitySearchService.ts
+++ src/services/CitySearchService.ts
@@ -6,4 +6,22 @@ export class CitySearchService {
 
     // Track whether the data is already loaded
     private isLoaded = false;
+
+    async loadCities(): Promise<void> {
+        if (this.isLoaded) return;
+        this.isLoaded = true; // ! delete later
+
+        try {
+            console.log('Fetching cities...');
+
+            const response = await fetch(API_CONFIG.citiesEndpoint);
+            console.log(response);
+
+            response.json
+            // TODO: parse JSON
+        } catch (error) {
+            console.error('Failed to load cities data:', error);
+            this.cities = [];
+        }
+    }    
 }
